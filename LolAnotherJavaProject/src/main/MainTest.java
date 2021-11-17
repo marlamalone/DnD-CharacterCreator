c9cbd970a0b3be8cbe7e64cb6dfed94a9904cbd3
Marla Malone <marla.r.malone@gmail.com>
2021-11-17 14:39:58 -0600

    Add files via upload

---
+++ LolAnotherJavaProject/src/main/MainTest.java
@@ -0,0 +1,29 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    final void testDnDrace() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    final void testDnDclass() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    final void testDnDname() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    @Test
+    final void testMain() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
